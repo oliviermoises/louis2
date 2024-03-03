524bfa5372aa9db5aead4a8bb6f131165f4b2880
Olivier Moises <olivier@generic-concept.com>
2024-03-03 20:22:17 +0100

    work in progress

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 curl http://localhost:8000/api/persons
 curl -X DELETE http://localhost:8000/api/persons/1
 curl -X POST 'http://localhost:8000/api/persons' -H 'Content-Type: application/json' -d '{"id":0, "name" : "olivier"}'
+
+
+curl -v -X GET http://localhost:8000/api/obtient_pret -H 'Content-Type: application/json' -d '{ "name" : "zolivier"}'
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
-from typing import Union
+from typing import Annotated, Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 
 from src.database import Database
 from src.person import Person
@@ -10,10 +10,12 @@ app = FastAPI()
 
 database = Database()
 
+
 # renvoie toutes les personnes
 @app.get("/api/persons")
 def list_persons():
     return database.list_records()
+
 
 # renvoie une personne dont l'id est passé
 @app.get("/api/persons/{person_id}")
@@ -26,6 +28,29 @@ def read_person(person_id: int):
 def delete_person(person_id: int):
     database.delete(person_id)
 
+
 @app.post("/api/persons")
-def create_person(person:Person):
+def create_person(person: Person):
     database.create(person)
+
+
+# si utilisation du get
+@app.get("/api/obtient_pret")
+def obtient_pret(person: Person):
+    if person.name[0] == "o":
+        return True
+    return False
+
+
+# si utilisation du Form HTML
+# https://fastapi.tiangolo.com/tutorial/request-forms/
+@app.post("/api/obtient_pret")
+def login(
+    ch1: Annotated[str, Form()],
+    ch2: Annotated[str, Form()],
+    ch3: Annotated[str, Form()],
+    ch4: Annotated[str, Form()],
+    ch5: Annotated[str, Form()],
+    ch6: Annotated[str, Form()],
+):
+    return True
